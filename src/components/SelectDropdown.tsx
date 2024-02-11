b83adf10dc0b65f5e08c0bd1fb4c5d8252639826
Zeha <zehairawan@gmail.com>
2024-02-11 11:09:43 +0800

    Setup story for SelectDropdown

--- src/components/SelectDropdown.tsx
+++ src/components/SelectDropdown.tsx
@@ -7,16 +7,16 @@ interface Option {
 
 interface Props {
   options?: Option[];
+  placeholder?: string;
 }
 
-const SelectDropdown: React.FC<Props> = ({ options = [] }) => {
+const SelectDropdown: React.FC<Props> = ({
+  options = [],
+  placeholder = "",
+}) => {
   const [dropdownOptions, setDropdownOptions] = useState(options);
   const [searchString, setSearchString] = useState("");
   const [isFocused, setIsFocused] = useState(false);
-
-  const handleSearchString = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchString(event.target.value);
-  };
 
   const handleFocus = () => {
     setIsFocused(true);
@@ -26,18 +26,30 @@ const SelectDropdown: React.FC<Props> = ({ options = [] }) => {
     setIsFocused(false);
   };
 
+  const handleSearchString = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearchString(value);
+
+    // Filter options based on the search string
+    const filteredOptions = options.filter((option) =>
+      option.label.toLowerCase().includes(value.toLowerCase()),
+    );
+    setDropdownOptions(filteredOptions);
+  };
+
   return (
     <div>
       <input
         type="text"
         value={searchString}
-        onChange={handleSearchString}
         onFocus={handleFocus}
         onBlur={handleBlur}
         style={{
           width: "100%",
           height: "2rem",
         }}
+        onChange={handleSearchString}
+        placeholder={placeholder}
       />
       {isFocused &&
         dropdownOptions.map((option) => (
